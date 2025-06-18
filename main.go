c75c1c17f3a1ed283114ce84bbd35280c3321e3d
Dmitriy S <XYZmail@yandex.ru>
2025-06-18 17:14:15 +0400

    create server with static files

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	_ "encoding/json"
+	"fmt"
+	"go_final_project/internal/server"
+	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Статические файлы
+	setupStaticFiles(r)
+
+	port := server.DefaultPort
+	err := http.ListenAndServe(":"+port, r)
+
+	if err != nil {
+		fmt.Printf("Ошибка 001 при запуске сервера: %s", err.Error())
+		return
+	}
+
+	fmt.Printf("Сервер запущен на порту :%s", port)
+
+}
+
+func setupStaticFiles(r *chi.Mux) {
+	root := http.Dir(server.WebDir)
+	fs := http.FileServer(root)
+
+	r.Handle("/*", fs)
+	r.Handle("/css/*", http.StripPrefix("/css/", http.FileServer(http.Dir(filepath.Join(server.WebDir, "css")))))
+	r.Handle("/js/*", http.StripPrefix("/js/", http.FileServer(http.Dir(filepath.Join(server.WebDir, "js")))))
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(server.WebDir, "favicon.ico"))
+	})
+
+}
+
+// func getTask(w http.ResponseWriter, r *http.Request) {
+// 	resp, err := []byte{'a', 'b', 'c'}, json.RawMessage([]byte{'1', '2'})
+
+// 	//json.Marshal(artists)
+
+// 	if err != nil {
+// 		http.Error(w, "erorroro 999", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	w.WriteHeader(http.StatusOK)
+
+// 	w.Write(resp)
+// }
